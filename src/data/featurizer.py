76262c2d3df792815368a04e2063769769f2d620
Nikunj Pradhan <nikunj.pradhan31@gmail.com>
2026-04-22 17:12:35 -0700

    featurizer

--- src/data/featurizer.py
+++ src/data/featurizer.py
@@ -3,82 +3,63 @@ from torch_geometric.data import Data
 from rdkit import Chem
 
 
-# def get_node_features(atom):
-#     """
-#     Extract node features from an RDKit atom.
-#     Features: atomic number, degree, hybridization, aromaticity, total valence, formal charge, radical electrons.
-#     """
-#     atomic_num = atom.GetAtomicNum()
-#     degree = atom.GetDegree()
+from rdkit import Chem
 
-#     try:
-#         hybridization = int(atom.GetHybridization())
-#     except:
-#         hybridization = 0
-
-#     is_aromatic = int(atom.GetIsAromatic())
-#     total_valence = atom.GetTotalValence()
-#     formal_charge = atom.GetFormalCharge()
-#     radical_electrons = atom.GetNumRadicalElectrons()
-
-#     return [
-#         float(atomic_num),
-#         float(degree),
-#         float(hybridization),
-#         float(is_aromatic),
-#         float(total_valence),
-#         float(formal_charge),
-#         float(radical_electrons),
-#     ]
+def one_hot_encoding(x, allowable_set):
+    if x not in allowable_set:
+        x = allowable_set[-1]
+    return [int(x == s) for s in allowable_set]
 
 
-# def get_edge_features(bond):
-#     """
-#     Extract edge features from an RDKit bond.
-#     Features: bond type (as float), is in ring, is conjugated.
-#     """
-#     bond_type = bond.GetBondTypeAsDouble()
-#     is_in_ring = int(bond.IsInRing())
-#     is_conjugated = int(bond.GetIsConjugated())
+def get_node_features(atom):
+    # --- Atomic number (bucketed instead of raw) ---
+    atomic_num = one_hot_encoding(
+        atom.GetAtomicNum(),
+        [1, 6, 7, 8, 9, 15, 16, 17, 35, 53, 0]  # common + unknown
+    )
 
-#     return [float(bond_type), float(is_in_ring), float(is_conjugated)]
+    # --- Degree ---
+    degree = one_hot_encoding(atom.GetDegree(), [0, 1, 2, 3, 4, 5])
 
-def get_edge_features(bond):
-    """
-    Tox21-optimized edge features for molecular graphs.
-    """
-
-    # Bond type encoding (more stable than raw float alone)
-    bond_type = bond.GetBondType()
-    bond_type_onehot = [
-        int(bond_type == Chem.rdchem.BondType.SINGLE),
-        int(bond_type == Chem.rdchem.BondType.DOUBLE),
-        int(bond_type == Chem.rdchem.BondType.TRIPLE),
-        int(bond_type == Chem.rdchem.BondType.AROMATIC),
-    ]
-
-    is_in_ring = int(bond.IsInRing())
-    is_conjugated = int(bond.GetIsConjugated())
-
-    # Directionality (important for stereochemistry-aware tasks)
-    is_stereo = int(bond.GetStereo() != Chem.rdchem.BondStereo.STEREONONE)
-
-    # Aromatic flag (redundant but useful explicitly at edge level)
-    is_aromatic = int(bond.GetIsAromatic())
-
-    # Conformational flexibility proxy
-    is_rotatable = int(bond.GetBondType() == Chem.rdchem.BondType.SINGLE and not bond.IsInRing())
-
-    return (
-        bond_type_onehot +
+    hybridization = one_hot_encoding(
+        atom.GetHybridization(),
         [
-            float(is_in_ring),
-            float(is_conjugated),
-            float(is_stereo),
-            float(is_aromatic),
-            float(is_rotatable),
+            Chem.rdchem.HybridizationType.SP,
+            Chem.rdchem.HybridizationType.SP2,
+            Chem.rdchem.HybridizationType.SP3,
+            Chem.rdchem.HybridizationType.SP3D,
+            Chem.rdchem.HybridizationType.SP3D2,
+            "other"
         ]
     )
+
+    formal_charge = one_hot_encoding(atom.GetFormalCharge(), [-2, -1, 0, 1, 2])
+    num_h = one_hot_encoding(atom.GetTotalNumHs(), [0, 1, 2, 3, 4])
+    aromatic = [int(atom.GetIsAromatic())]
+    in_ring = [int(atom.IsInRing())]
+    chirality = one_hot_encoding(
+        str(atom.GetChiralTag()),
+        [
+            "CHI_UNSPECIFIED",
+            "CHI_TETRAHEDRAL_CW",
+            "CHI_TETRAHEDRAL_CCW",
+            "other"
+        ]
+    )
+
+    radical = [atom.GetNumRadicalElectrons()]
+    features = (
+        atomic_num +
+        degree +
+        hybridization +
+        formal_charge +
+        num_h +
+        aromatic +
+        in_ring +
+        chirality +
+        radical
+    )
+    return list(map(float, features))
 
 
 def get_edge_features(bond):
@@ -178,7 +159,7 @@ class MolFeaturizer:
             edge_attr = torch.tensor(edge_features, dtype=torch.float)
         else:
             edge_index = torch.empty((2, 0), dtype=torch.long)
-            edge_attr = torch.empty((0, 3), dtype=torch.float)
+            edge_attr = torch.empty((0, 9), dtype=torch.float)
 
         data = Data(x=x, edge_index=edge_index, edge_attr=edge_attr, z=z_tensor)
 
